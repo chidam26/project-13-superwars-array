4a7e82a7c025911e96c924f67a098c7de2867bcb
chidam26 <chidam269@gmail.com>
2021-09-27 18:14:32 +0530

    superwars3

--- src/app.js
+++ src/app.js
@@ -23,10 +23,19 @@ const PLAYERS = [
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
-
+    
     // Instead of forloop use Map method
     // Code here
+    let playerDetails = "";
+    playerDetails = PLAYERS.map((player, i) => {
+      console.log(i);
+      return {
+        name: player,
+        image: "images/super-" + i + ".png",
+        strength: getRandomStrength(),
+        type: i % 2 == 0 ? "hero" : "villain"
+      };
+    });
 
     return detailedPlayers;
 }
@@ -43,6 +52,14 @@ const buildPlayers = (players, type) => {
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
+    fragment = players.filter(player => player.type == type).map(player =>
+        `<div class="player">
+         <img src="${player.image}" alt=" ">
+         <div class="name">${player.name}</div>
+         <div class="strength">${player.strength}</div>
+        </div>`
+    )
+    .join();
 
     return fragment;
 }
